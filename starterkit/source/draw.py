42c2e7ed2ca6bbfc56b178f08ccd4ca9e60cd206
9B7C <16whitijarl@washk12.org>
2014-01-30 11:32:25 -0700

    More organized
    It now explains what the function does

--- starterkit/source/draw.py
+++ starterkit/source/draw.py
@@ -5,69 +5,38 @@ from sprite import Sprite
 
 import random
 
+ri=random.randint
+ld_esrsfr_23srg4_d=game
+
 for i in range(100):
-    random.seed(random.randint(1,10000000000))
+    random.seed(ri(1,10000000000))
 
-colors=[random.randint(0,255),random.randint(0,255),random.randint(0,255)]
-accels=[0,0,0]
+ser_32=[ri(0,255),ri(0,255),ri(0,255)]
+wk_bgeriw2_d=[0,0,0]
 
-sizes=[random.randint(10,1280),random.randint(10,720)]
-accels1=[0,0]
-
-interval=1
-interval2=5
-
-maxint=5
-maxint2=25
-
-def solve(num):
-    i=colors[num]
-    v=accels[num]
-    colors[num]+=v
-    if colors[num]>255:
-        colors[num]=255
-    elif colors[num]<0:
-        colors[num]=0
-    accels[num]+=random.randint(-interval,interval)
-    if accels[num]>maxint:
-        accels[num]=maxint
-    elif accels[num]<-maxint:
-        accels[num]=-maxint
-
-def solve2(num,min1,max1):
-    i=sizes[num]
-    v=accels1[num]
-    sizes[num]+=v
-    if sizes[num]>max1:
-        sizes[num]=max1
-    elif sizes[num]<min1:
-        sizes[num]=min1
-    accels1[num]+=random.randint(-interval2,interval2)
-    if accels1[num]>maxint2:
-        accels1[num]=maxint2
-    elif accels1[num]<-maxint2:
-        accels1[num]=-maxint2
+def avewsae_2t3(dvsresr_yj3,sergewf_2324sdfw3d123f23f3df_wf4,seg_43ge5_hg,erger_34fh_wln,ser_ervrteedwef_rge5r,dsr_ef4ed,hrtkewu3_h,dsr_eh4ed): 
+	#This adds to the current colors, and adds to the acceleration as well.
+    sergewf_2324sdfw3d123f23f3df_wf4[dvsresr_yj3]+=seg_43ge5_hg[dvsresr_yj3]
+    if sergewf_2324sdfw3d123f23f3df_wf4[dvsresr_yj3]>dsr_ef4ed:
+        sergewf_2324sdfw3d123f23f3df_wf4[dvsresr_yj3]=dsr_ef4ed
+    elif sergewf_2324sdfw3d123f23f3df_wf4[dvsresr_yj3]<ser_ervrteedwef_rge5r:
+        sergewf_2324sdfw3d123f23f3df_wf4[dvsresr_yj3]=ser_ervrteedwef_rge5r
+    seg_43ge5_hg[dvsresr_yj3]+=ri(-dsr_eh4ed,dsr_eh4ed)
+    if seg_43ge5_hg[dvsresr_yj3]>hrtkewu3_h:
+        seg_43ge5_hg[dvsresr_yj3]=hrtkewu3_h
+    elif seg_43ge5_hg[dvsresr_yj3]<-hrtkewu3_h:
+        seg_43ge5_hg[dvsresr_yj3]=-hrtkewu3_h
 
 def draw():
-    """
-    Drawing logic
-    """
 
-    # Fill the screen with black
-    solve(0)
-    solve(1)
-    solve(2)
-    solve2(0,10,1280)
-    solve2(1,10,720)
-    # game.screen = pygame.display.set_mode((sizes[0],sizes[1]),pygame.DOUBLEBUF)
-    background = pygame.Surface(game.screen.get_size())
-    background = background.convert()
-    game.screen.blit(background,(0,0))
-    game.screen.fill((colors[0],colors[1],colors[2]))
-    
-    # insert logic here
+    for i in range(3):
+        avewsae_2t3(i-1,ser_32,wk_bgeriw2_d,ri(-200,200),0,255,5,1)
 
-    # Don't change these
+    sbde_2brdfh5_dfe = pygame.Surface(ld_esrsfr_23srg4_d.screen.get_size())
+    sbde_2brdfh5_dfe = sbde_2brdfh5_dfe.convert()
+    ld_esrsfr_23srg4_d.screen.blit(sbde_2brdfh5_dfe,(0,0))
+    ld_esrsfr_23srg4_d.screen.fill((ser_32[0],ser_32[1],ser_32[2]))
+
     pygame.display.update()
     pygame.display.flip()
 
